6f18e9a0d59459a1e4aeb3ab8402922eec4b4185
lepetitprinz <lepetitprinz0911@gmail.com>
2021-05-19 21:53:44 +0900

    add source

---
+++ practice/filter.scala
@@ -0,0 +1,51 @@
+import org.apache.spark._
+import org.apache.log4j._
+import scala.math.min
+
+object MinTemperatures{
+
+
+	def parseLine(line: String): (String, String, FLoat) = {
+		val fileds = line.split(",")
+		val stationID = fileds(0)
+		val entryType = fileds(2)
+		val temperature = fileds(3).toFloat * 0.1f * (9.0f / 5.0f) + 32.0f
+		(stationID, entryType, temperature)
+	}
+
+	def main(args: Array[String]) {
+
+		// Set the log level to only print errors
+		Logger.getLogger("org").setLevel(Level.ERROR)
+
+		// Create a SparkContext
+		val sc = new SparkContext("local[*]", "MinTemperatures")
+
+		// Read each line of input data
+		val lines = sc.textFile("data/1800.csv")
+
+		// Read each line of input data
+		val parseLines = lines.map(parseLine)
+
+		// Filter out all but TMIN entries
+		val minTemps = parseLines.filter(x => x._2 == "TMIN")
+
+		// Convert to (stationID, temperature)
+		val stationTemps = minTemps.map(x => (x._1, x._3.toFloat))
+
+		// Reduce by stationID retating the minimum temperature
+		val MinTempByStation = stationTemps.reduceByKey((x, y) => min(x, y))
+
+		// Collect, format, and print the results
+		val results = MinTempByStation.collect()
+
+		//
+		for (result <- results.sorted) {
+			val station = result._1
+			val temp = result._2
+			val formattedTemp = f"$temp%.2f F"
+			print(s"$station minimum temperature: $formattedTemp")
+		}
+	}
+
+}
